5327380c303bcbc64f79a17fe13ddda8b6710b8d
eric trotchie <Eric-JT@users.noreply.github.com>
2017-05-13 19:20:51 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var callback = function(){
+
+  const inputs = document.querySelectorAll('#controls input');
+
+  function handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+  }
+
+  inputs.forEach(input => input.addEventListener('change', handleUpdate));
+  inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+};
+
+document.onreadystatechange = function () {
+  if (document.readyState === "complete") {
+    callback();
+  }
+}
